ca17b78d30388d6878549ff7dc433a91d3a7a385
Giovanni Ferri <giovanni@syscode.uk>
2026-07-16 00:52:57 +0100

    Update quickserve to 0.1.13

--- Formula/quickserve.rb
+++ Formula/quickserve.rb
@@ -1,30 +1,30 @@
 class Quickserve < Formula
   desc "Tiny HTTP directory server with LAN, UPnP, and Cloudflare Tunnel support"
   homepage "https://github.com/syscode-labs/quickserve"
-  version "0.1.12"
+  version "0.1.13"
   license "CC0-1.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/syscode-labs/quickserve/releases/download/v0.1.12/quickserve_v0.1.12_darwin_arm64.tar.gz"
-      sha256 "71cd01613d8928e537314d51dbf173cf6066b24a9ddd76ab1eb91318d677f1f6"
+      url "https://github.com/syscode-labs/quickserve/releases/download/v0.1.13/quickserve_v0.1.13_darwin_arm64.tar.gz"
+      sha256 "d51a76ca5f24c7ce0520b48fa3e9b9baed4defda0df32f972efed8555277c628"
     end
 
     on_intel do
-      url "https://github.com/syscode-labs/quickserve/releases/download/v0.1.12/quickserve_v0.1.12_darwin_amd64.tar.gz"
-      sha256 "95904eeeae9b16a40cf7d9e2f194a20ff02a2fd0ad3696703b46126d693aab36"
+      url "https://github.com/syscode-labs/quickserve/releases/download/v0.1.13/quickserve_v0.1.13_darwin_amd64.tar.gz"
+      sha256 "53d25f54adbffb23679234191882cd27db68db905ec85f11fd5e86f73932a41d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/syscode-labs/quickserve/releases/download/v0.1.12/quickserve_v0.1.12_linux_arm64.tar.gz"
-      sha256 "83081cf254c69d50031b68e6a1605f072b52edb975cfcf5d9520c593adb942dc"
+      url "https://github.com/syscode-labs/quickserve/releases/download/v0.1.13/quickserve_v0.1.13_linux_arm64.tar.gz"
+      sha256 "ad946bb6d264edbfe68ab2cf7e322fa7024a83bfe08ef22a084b0f9a81f6e331"
     end
 
     on_intel do
-      url "https://github.com/syscode-labs/quickserve/releases/download/v0.1.12/quickserve_v0.1.12_linux_amd64.tar.gz"
-      sha256 "0132b6de5ad9bafa1aeaa7b00530fc580fb16c49480026b940b0ff4f8305c869"
+      url "https://github.com/syscode-labs/quickserve/releases/download/v0.1.13/quickserve_v0.1.13_linux_amd64.tar.gz"
+      sha256 "e7059ffb17698bb493a84f2fbc3bdc808d2497f775182b5953991df0c0588d71"
     end
   end
 
